a4bb81eff323a9950263b5123d8833eb7a89439c
adrian garcia garro <adrian.garcia@odilo.us>
2021-08-25 13:05:12 +0200

    Added the Bank application generated to springInitializer

---
+++ apps/bank-app/src/test/java/com/iobuilders/apps/bank/BankAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.iobuilders.apps.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration(classes = BankAppApplication.class)
+@SpringBootTest
+class BankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
